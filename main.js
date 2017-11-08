dfd450def85ec6df56c3025977dce69a0ce79878
Terrance Corley <terrancecorley@outlook.com>
2017-11-08 07:04:00 -0800

    Added js gallery functionality

--- main.js
+++ main.js
@@ -18,30 +18,66 @@ document.addEventListener('DOMContentLoaded', () => {
 			
 			// when all btn clicked, show all food items
 			if (currentBtn.textContent.toUpperCase() === 'ALL') {
-				bfastItems.style.display = 'initial';
-				lunchItems.style.display = 'initial';
-				dinnerItems.style.display = 'initial';
+				for (let i = 0; i < bfastItems.length; i += 1) {
+					let bfastItem = bfastItems[i];
+					bfastItem.style.display = 'initial';
+				}
+				for (let i = 0; i < lunchItems.length; i += 1) {
+					let lunchItem = lunchItems[i];
+					lunchItem.style.display = 'initial';
+				}
+				for (let i = 0; i < dinnerItems.length; i += 1) {
+					let dinnerItem = dinnerItems[i];
+					dinnerItem.style.display = 'initial';
+				}
 			}
 
 			// when bfast btn clicked, show bfast, hide lunch & dinner items
 			if (currentBtn.textContent.toUpperCase() === 'BREAKFAST') {
-				bfastItems.style.display = 'initial';
-				lunchItems.style.display = 'none';
-				dinnerItems.style.display = 'none';
+				for (let i = 0; i < bfastItems.length; i += 1) {
+					let bfastItem = bfastItems[i];
+					bfastItem.style.display = 'initial';
+				}
+				for (let i = 0; i < lunchItems.length; i += 1) {
+					let lunchItem = lunchItems[i];
+					lunchItem.style.display = 'none';
+				}
+				for (let i = 0; i < dinnerItems.length; i += 1) {
+					let dinnerItem = dinnerItems[i];
+					dinnerItem.style.display = 'none';
+				}
 			}
 
 			// when lunch btn clicked, show lunch, hide bfast & dinner items
-			if (currentBtn.textContent.toUpperCase() === 'BREAKFAST') {
-				bfastItems.style.display = 'none';
-				lunchItems.style.display = 'initial';
-				dinnerItems.style.display = 'none';
+			if (currentBtn.textContent.toUpperCase() === 'LUNCH') {
+				for (let i = 0; i < bfastItems.length; i += 1) {
+					let bfastItem = bfastItems[i];
+					bfastItem.style.display = 'none';
+				}
+				for (let i = 0; i < lunchItems.length; i += 1) {
+					let lunchItem = lunchItems[i];
+					lunchItem.style.display = 'initial';
+				}
+				for (let i = 0; i < dinnerItems.length; i += 1) {
+					let dinnerItem = dinnerItems[i];
+					dinnerItem.style.display = 'none';
+				}
 			}
 
 			// when dinner btn clicked, show dinner, hide bfast & lunch items
-			if (currentBtn.textContent.toUpperCase() === 'BREAKFAST') {
-				bfastItems.style.display = 'none';
-				lunchItems.style.display = 'none';
-				dinnerItems.style.display = 'initial';
+			if (currentBtn.textContent.toUpperCase() === 'DINNER') {
+				for (let i = 0; i < bfastItems.length; i += 1) {
+					let bfastItem = bfastItems[i];
+					bfastItem.style.display = 'none';
+				}
+				for (let i = 0; i < lunchItems.length; i += 1) {
+					let lunchItem = lunchItems[i];
+					lunchItem.style.display = 'none';
+				}
+				for (let i = 0; i < dinnerItems.length; i += 1) {
+					let dinnerItem = dinnerItems[i];
+					dinnerItem.style.display = 'initial';
+				}
 			} 
 		}
 	
